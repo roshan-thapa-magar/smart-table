6f61bb7d1edaea2adc0b6f708b6746a442d830c3
Roshan Thapa Magar <roshan@octspace.com>
2026-04-24 10:51:17 +0545

    table

--- components/pos/aviable-table.tsx
+++ components/pos/aviable-table.tsx
@@ -3,21 +3,20 @@
 import React from "react"
 import { TABLE_STATUS } from "@/constants/table-status"
 
-const tables = [
-  { id: 1, name: "T-1", status: "blank" },
-  { id: 2, name: "T-2", status: "new" },
-  { id: 3, name: "T-3", status: "preparing" },
-  { id: 4, name: "T-4", status: "ready" },
-  { id: 5, name: "T-5", status: "served" },
-  { id: 6, name: "T-6", status: "billing" },
-  { id: 7, name: "T-7", status: "new" },
-  { id: 8, name: "T-8", status: "ready" },
-  { id: 9, name: "T-9", status: "blank" },
-]
+// generate 40 tables
+const tables = Array.from({ length: 100 }, (_, i) => {
+  const statuses = ["blank", "new", "preparing", "ready", "served", "billing"]
+
+  return {
+    id: i + 1,
+    name: `T-${i + 1}`,
+    status: statuses[i % statuses.length],
+  }
+})
 
 export default function AviableTable() {
   return (
-    <div className="grid custom-grid grid-cols-4 sm:grid-cols-6  lg:grid-cols-4 2xl:grid-cols-8 gap-4 p-2">
+    <div className="grid custom-grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-4 2xl:grid-cols-8 gap-4 p-2 ">
       {tables.map((table) => {
         const status =
           TABLE_STATUS[table.status as keyof typeof TABLE_STATUS]
